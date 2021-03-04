11834d09cb53898a7361c756b31967f398ba83d3
presidentK <1094590956@qq.com>
2021-03-04 14:34:51 +0800

    modify

---
+++ src/scene/IndexScene.ts
@@ -0,0 +1,10 @@
+class IndexScene extends BaseScene{
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		
+	}
+}
--- src/scene/LoginScene.ts
+++ src/scene/LoginScene.ts
@@ -1,4 +1,8 @@
 class LoginScene extends BaseScene {
+    private progressBar:eui.ProgressBar;
+    private isLogining:boolean=false;
+    private timer:egret.Timer;
+
 	public constructor() {
 		super();
 	}
@@ -9,8 +13,22 @@ class LoginScene extends BaseScene {
 		if(partName&&instance.touchEnabled){
             instance.addEventListener(egret.TouchEvent.TOUCH_TAP,()=>{
                 switch(partName){
-                    case 'label1':
-                        egret.log('[INFO]:label1 clicked');
+                    case 'loginBtn':
+                        if(this.isLogining) return;
+                        this.isLogining=true;
+                        let progressValue=0;
+                        this.timer = new egret.Timer(100, 0);
+                        this.timer.addEventListener(egret.TimerEvent.TIMER, ()=>{
+                            if(progressValue<100){
+                                progressValue+=Math.round(Math.random()*4);
+                                if(progressValue>100) progressValue=100;
+                            }else{
+                                this.timer.stop();
+                                SceneManager.go(IndexScene);
+                            }
+                            this.progressBar.value=progressValue;
+                        }, this);
+                        this.timer.start();
                         break;
                     default:
                         break;
